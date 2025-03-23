adf95c5e99a57f5ead140a6c120c7a5c866399f4
leo997a <166352340+leo997a@users.noreply.github.com>
2025-03-23 06:11:41 +0300

    Update reok.py

--- reok.py
+++ reok.py
@@ -1156,8 +1156,8 @@ def progressive_carry(ax, team_name, col, phase_tag):
             st.dataframe(away_proc, hide_index=True)
             
     if an_tp == 'Shotmap':
-    st.header(reshape_arabic_text('خريطة التسديدات'))
-    st.header(f'{an_tp}')
+        st.header(reshape_arabic_text('خريطة التسديدات'))
+        st.header(f'{an_tp}')
 
     def plot_ShotsMap(ax, team_name, col, phase_tag):
         if phase_tag == 'Full Time':
