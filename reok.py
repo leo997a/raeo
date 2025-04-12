ae73db1488d4606d840771a5f4a4c542c4152ce9
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-12 05:18:16 +0300

    Update reok.py

--- reok.py
+++ reok.py
@@ -955,95 +955,102 @@ if match_url and st.session_state.confirmed:
             # st.header(f'{st.session_state.analysis_type}')
             st.header(f'{an_tp}')
             
-            def progressive_pass(ax, team_name, col, phase_tag):
-                if phase_tag == 'Full Time':
-                    df_prop = df[(df['teamName']==team_name) & (df['outcomeType']=='Successful') & (df['prog_pass']>9.144) & (~df['qualifiers'].str.contains('Corner|Freekick')) & (df['x']>=35)]
-                elif phase_tag == 'First Half':
-                    df_fh = df[df['period'] == 'FirstHalf']
-                    df_prop = df_fh[(df_fh['teamName']==team_name) & (df_fh['outcomeType']=='Successful') & (df_fh['prog_pass']>9.11) & (~df_fh['qualifiers'].str.contains('Corner|Freekick')) & (df_fh['x']>=35)]
-                elif phase_tag == 'Second Half':
-                    df_sh = df[df['period'] == 'SecondHalf']
-                    df_prop = df_sh[(df_sh['teamName']==team_name) & (df_sh['outcomeType']=='Successful') & (df_sh['prog_pass']>9.11) & (~df_sh['qualifiers'].str.contains('Corner|Freekick')) & (df_sh['x']>=35)]
-                
+def progressive_pass(ax, team_name, col, phase_tag):
+    if phase_tag == 'Full Time':
+        df_prop = df[(df['teamName'] == team_name) & (df['outcomeType'] == 'Successful') & (df['prog_pass'] > 9.144) & (~df['qualifiers'].str.contains('Corner|Freekick')) & (df['x'] >= 35)]
+    elif phase_tag == 'First Half':
+        df_fh = df[df['period'] == 'FirstHalf']
+        df_prop = df_fh[(df_fh['teamName'] == team_name) & (df_fh['outcomeType'] == 'Successful') & (df_fh['prog_pass'] > 9.144) & (~df_fh['qualifiers'].str.contains('Corner|Freekick')) & (df_fh['x'] >= 35)]
+    elif phase_tag == 'Second Half':
+        df_sh = df[df['period'] == 'SecondHalf']
+        df_prop = df_sh[(df_sh['teamName'] == team_name) & (df_sh['outcomeType'] == 'Successful') & (df_sh['prog_pass'] > 9.144) & (~df_sh['qualifiers'].str.contains('Corner|Freekick')) & (df_sh['x'] >= 35)]
+
     pitch = VerticalPitch(pitch_type='uefa', corner_arcs=True, pitch_color=bg_color, line_color=line_color, line_zorder=3, linewidth=2)
     pitch.draw(ax=ax)
 
-            
-    left_prop = df_prop[df_prop['y']>136/3]
-    midd_prop = df_prop[(df_prop['y']<=136/3) & (df_prop['y']>=68/3)]
-    rigt_prop = df_prop[df_prop['y']<68/3]
-            
-    if len(df_prop) != 0:
+    left_prop = df_prop[df_prop['y'] > 136/3]
+    midd_prop = df_prop[(df_prop['y'] <= 136/3) & (df_prop['y'] >= 68/3)]
+    rigt_prop = df_prop[df_prop['y'] < 68/3]
+
+    total_prop = len(df_prop)
+    prop_left = len(left_prop)
+    prop_mid = len(midd_prop)
+    prop_right = len(rigt_prop)
+
+    prop_left_per = round((prop_left / total_prop * 100), 2) if total_prop > 0 else 0
+    prop_mid_per = round((prop_mid / total_prop * 100), 2) if total_prop > 0 else 0
+    prop_right_per = round((prop_right / total_prop * 100), 2) if total_prop > 0 else 0
+
+    if total_prop != 0:
         name_counts = df_prop['shortName'].value_counts()
         name_counts_df = name_counts.reset_index()
         name_counts_df.columns = ['name', 'count']
-        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)  
+        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)
         name_counts_df_show = name_counts_df.reset_index(drop=True)
         most_name = name_counts_df_show['name'][0]
         most_count = name_counts_df_show['count'][0]
     else:
-                most_name = 'None'
-                most_count = 0  
-                
-    if len(left_prop) != 0:
+        most_name = 'None'
+        most_count = 0
+
+    if prop_left != 0:
         name_counts = left_prop['shortName'].value_counts()
         name_counts_df = name_counts.reset_index()
         name_counts_df.columns = ['name', 'count']
-        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)  
-        name_counts_df = name_counts_df.reset_index()
+        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)
         l_name = name_counts_df['name'][0]
         l_count = name_counts_df['count'][0]
     else:
         l_name = 'None'
-        l_count = 0   
-            
-    if len(midd_prop) != 0:
+        l_count = 0
+
+    if prop_mid != 0:
         name_counts = midd_prop['shortName'].value_counts()
         name_counts_df = name_counts.reset_index()
         name_counts_df.columns = ['name', 'count']
-        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)  
-        name_counts_df = name_counts_df.reset_index()
+        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)
         m_name = name_counts_df['name'][0]
         m_count = name_counts_df['count'][0]
     else:
-            m_name = 'None'
-            m_count = 0   
-            
-    if len(rigt_prop) != 0:
+        m_name = 'None'
+        m_count = 0
+
+    if prop_right != 0:
         name_counts = rigt_prop['shortName'].value_counts()
         name_counts_df = name_counts.reset_index()
         name_counts_df.columns = ['name', 'count']
-        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)  
-        name_counts_df = name_counts_df.reset_index()
+        name_counts_df = name_counts_df.sort_values(by='count', ascending=False)
         r_name = name_counts_df['name'][0]
         r_count = name_counts_df['count'][0]
     else:
         r_name = 'None'
-        r_count = 0   
-            
+        r_count = 0
+
     pitch.lines(df_prop.x, df_prop.y, df_prop.endX, df_prop.endY, comet=True, lw=4, color=col, ax=ax)
     pitch.scatter(df_prop.endX, df_prop.endY, s=75, zorder=3, color=bg_color, ec=col, lw=1.5, ax=ax)
-            
+
     if phase_tag == 'Full Time':
-        ax.text(34, 116, 'Full Time: 0-90 minutes', color=col, fontsize=13, ha='center', va='center')
+        ax.text(34, 116, reshape_arabic_text('الوقت الكامل: 0-90 دقيقة'), color=col, fontsize=13, ha='center', va='center')
     elif phase_tag == 'First Half':
-        ax.text(34, 116, 'First Half: 0-45 minutes', color=col, fontsize=13, ha='center', va='center')
+        ax.text(34, 116, reshape_arabic_text('الشوط الأول: 0-45 دقيقة'), color=col, fontsize=13, ha='center', va='center')
     elif phase_tag == 'Second Half':
-        ax.text(34, 116, 'Second Half: 45-90 minutes', color=col, fontsize=13, ha='center', va='center')
-        ax.text(34, 112, f'Open-Play Progressive Passes: {len(df_prop)}', color=col, fontsize=13, ha='center', va='center')
-        ax.text(34, 108, f'Most by: {most_name}({most_count})', color=col, fontsize=13, ha='center', va='center')
-            
-        ax.text(10, 10, f'التقدم بالكرة من الجهة اليسرى\n{prop_left} تقدم ({prop_left_per}%)', color=col, fontsize=12, ha='center', va='center')
-        ax.text(58, 10, f'التقدم بالكرة من الجهة اليمنى\n{prop_right} تقدم ({prop_right_per}%)', color=col, fontsize=12, ha='center', va='center')
-            
-        ax.text(340/6, -5, f'From Left: {len(left_prop)}', color=col, ha='center', va='center')
-        ax.text(34, -5, f'From Mid: {len(midd_prop)}', color=col, ha='center', va='center')
-        ax.text(68/6, -5, f'From Right: {len(rigt_prop)}', color=col, ha='center', va='center')
-            
-        ax.text(340/6, -7, f'Most by:\n{l_name}({l_count})', color=col, ha='center', va='top')
-        ax.text(34, -7, f'Most by:\n{m_name}({m_count})', color=col, ha='center', va='top')
-        ax.text(68/6, -7, f'Most by:\n{r_name}({r_count})', color=col, ha='center', va='top')
-                 
+        ax.text(34, 116, reshape_arabic_text('الشوط الثاني: 45-90 دقيقة'), color=col, fontsize=13, ha='center', va='center')
+
+    ax.text(34, 112, reshape_arabic_text(f'التمريرات التقدمية في اللعب المفتوح: {total_prop}'), color=col, fontsize=13, ha='center', va='center')
+    ax.text(34, 108, reshape_arabic_text(f'الأكثر: {most_name} ({most_count})'), color=col, fontsize=13, ha='center', va='center')
+
+    ax.text(10, 10, reshape_arabic_text(f'التقدم من الجهة اليسرى\n{prop_left} تقدم ({prop_left_per}%)'), color=col, fontsize=12, ha='center', va='center')
+    ax.text(34, 10, reshape_arabic_text(f'التقدم من الوسط\n{prop_mid} تقدم ({prop_mid_per}%)'), color=col, fontsize=12, ha='center', va='center')
+    ax.text(58, 10, reshape_arabic_text(f'التقدم من الجهة اليمنى\n{prop_right} تقدم ({prop_right_per}%)'), color=col, fontsize=12, ha='center', va='center')
+
+    ax.text(340/6, -5, reshape_arabic_text(f'من اليسار: {prop_left}'), color=col, ha='center', va='center')
+    ax.text(34, -5, reshape_arabic_text(f'من الوسط: {prop_mid}'), color=col, ha='center', va='center')
+    ax.text(68/6, -5, reshape_arabic_text(f'من اليمين: {prop_right}'), color=col, ha='center', va='center')
+
+    ax.text(340/6, -7, reshape_arabic_text(f'الأكثر:\n{l_name} ({l_count})'), color=col, ha='center', va='top')
+    ax.text(34, -7, reshape_arabic_text(f'الأكثر:\n{m_name} ({m_count})'), color=col, ha='center', va='top')
+    ax.text(68/6, -7, reshape_arabic_text(f'الأكثر:\n{r_name} ({r_count})'), color=col, ha='center', va='top')
+
     return name_counts_df_show
             
     pp_time_phase = st.pills(" ", ['Full Time', 'First Half', 'Second Half'], default='Full Time', key='pp_time_pill')
