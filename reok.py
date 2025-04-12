924087b8b625bd514cc8abad8dc1694b2aede8c3
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-12 04:45:31 +0300

    Update reok.py

--- reok.py
+++ reok.py
@@ -96,9 +96,9 @@ else:
 
 # إذا تم تأكيد الرابط، جلب البيانات
 if match_url and st.session_state.confirmed:
-    @st.cache_data
-    def get_event_data(match_url):
-        def extract_json_from_html(html_path):
+@st.cache_data
+def get_event_data(match_url):
+def extract_json_from_html(html_path):
         try:
             response = requests.get(html_path)
             response.raise_for_status()
