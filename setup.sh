18932126a9913ccc7d26ccc1d7c33258662d4bae
leo997a <166352340+leo997a@users.noreply.github.com>
2025-04-13 05:01:49 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 # تحديث قائمة الحزم
 apt-get update
-# تثبيت الأدوات المساعدة
-apt-get install -y wget gnupg unzip
+# تثبيت مكتبات النظام المطلوبة لـ Chrome وChromeDriver
+apt-get install -y \
+    wget \
+    gnupg \
+    unzip \
+    libglib2.0-0 \
+    libnss3 \
+    libgconf-2-4 \
+    libfontconfig1 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxi6 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxrandr2 \
+    libxtst6 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcairo2 \
+    libcups2 \
+    libdrm2 \
+    libgbm1 \
+    libgtk-3-0 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libasound2 \
+    fonts-liberation \
+    xdg-utils
 # إضافة مفتاح Google Chrome ومستودعه
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
